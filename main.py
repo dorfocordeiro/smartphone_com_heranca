2937e1d167df407b67b203429d01432363cdce03
Rodolfo Cordeiro <161094370+dorfocordeiro@users.noreply.github.com>
2024-06-28 10:57:23 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from eletronic import SmartPhone, Xiaomi
+print('Quero um PC novo')
+
+galaxy_s24 = SmartPhone('Samsung Galaxy S24 Ultra')
+iphone15 = SmartPhone('iPhone 15 Pro Max')
+
+galaxy_s24.ligar()
+print()
+iphone15.desligar()
+
+redminote13 = Xiaomi('Xiaomi Redmi Note 13')
+pocox6 = Xiaomi('Xiaomi Poco X6')
+
+redminote13.ligar()
+pocox6.desligar()
